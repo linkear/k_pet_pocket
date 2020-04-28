4edb26f43baf5aea4962d3ee376e837bbf1c98b9
Corian2108 <jefferson.ruiz2108@gmail.com>
2020-04-28 13:20:40 -0500

    Seagrega todas las funcionalidades de apariencia

---
+++ Registro/Codigo/js/businesstyle.js
@@ -0,0 +1,102 @@
+/*-----Select1-busines-----*/
+
+var businesSelect = document.getElementById('businesOptions')
+var businesNewInput = document.getElementById('hidenInput')
+var businesButton = document.getElementById('businesDropDown')
+
+businesSelect.addEventListener('click', () => {
+    if (businesSelect.value === 'Other') {
+        businesSelect.style.display = 'none'
+        businesNewInput.style.display = 'inline-block'
+        businesButton.style.display = 'inline-block'
+    } else {
+        return
+    }
+});
+
+businesButton.addEventListener('click', () => {
+    businesNewInput.style.display = 'none'
+    businesButton.style.display = 'none'
+    businesSelect.value = 'Veterinary'
+    businesSelect.style.display = 'inline-block'
+})
+
+/*-----Add-Social-----*/
+
+var socialContainer = document.getElementById('mediaContent')
+var form = document.getElementById('businesReg')
+var addButton = document.getElementById('addButton')
+var regButton = document.getElementById('button6')
+var footer = document.getElementById('footerBusines')
+var contador = 1;
+var ident = 0;
+
+var addNewSocial = function () {
+
+    if (contador === 10) {
+        return
+    } else {
+        contador += 1
+        newHeight = (70 + (65 * contador)) + 'px'
+        socialContainer.style.height = newHeight;
+        console.log('div rojo ' + newHeight);
+        formHeight = (670 + (65 * contador)) + 'px'
+        form.style.height = formHeight;
+        console.log('formulario ' + formHeight);
+        addButtonHeight = (690 + (65 * contador)) + 'px'
+        addButton.style.top = addButtonHeight;
+        console.log('add Button ' + addButtonHeight);
+        regButtonHeight = (810 + (65 * contador)) + 'px'
+        regButton.style.top = regButtonHeight;
+        console.log('reg Button ' + regButtonHeight);
+        footerHeight = (880 + (65 * contador)) + 'px'
+        footer.style.top = footerHeight;
+        console.log('footer ' + footerHeight);
+
+        newSocial = document.createElement('input')
+        socialContainer.appendChild(newSocial)
+        newSocial.className = 'socialLink';
+        newSocial.placeholder = 'Link'
+
+        newSelect = document.createElement('select')
+        socialContainer.appendChild(newSelect)
+        newSelect.className = 'socialOptions';
+
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Facebook'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Twittet'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('WhatsApp'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Instagram'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Pinterest'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Web'))
+        ident += 1
+        newSelect.appendChild(document.createElement('option')).
+        id = 'option' + ident;
+        document.getElementById('option' + ident).
+        appendChild(document.createTextNode('Other'))
+
+    }
+}
---
+++ Registro/Codigo/js/regist2style.js
@@ -0,0 +1,38 @@
+;
+/*-----Checkbox-----*/
+var checkbox = document.getElementById('noChip')
+var label = document.getElementById('noChipLabel')
+var chip = document.getElementById('input8')
+
+checkbox.addEventListener('click', () => {
+    if (checkbox.checked) {
+        label.style.setProperty('--bg-pseudo', 'url("../assets/icon/huella.png")')
+        chip.disabled = true
+    } else {
+        label.style.setProperty('--bg-pseudo', '')
+        chip.disabled = false
+    }
+
+});
+
+/*-----Select-regist2-----*/
+var select = document.getElementById('select')
+var newInput = document.getElementById('other')
+var newButton = document.getElementById('selectBack')
+
+select.addEventListener('click', () => {
+    if (select.value === 'Other') {
+        select.style.display = 'none'
+        newInput.style.display = 'inline-block'
+        newButton.style.display = 'inline-block'
+    } else {
+        return
+    }
+});
+
+newButton.addEventListener('click', () => {
+    newInput.style.display = 'none'
+    newButton.style.display = 'none'
+    select.value = 'Breed'
+    select.style.display = 'inline-block'
+});
